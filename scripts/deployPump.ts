dce75321cbf41170792452d74454f5dc0382d536
vvizardev <nakao95911@gmail.com>
2025-04-22 02:32:03 +0900

    config: deploy script

--- scripts/deployPump.ts
+++ scripts/deployPump.ts
@@ -5,7 +5,7 @@ import hre from "hardhat"
 async function main() {
 	console.log("Starting deployments")
 	const routerAddress = "0xfb8e1c3b833f9e67a71c859a132cf783b645e436"
-	const PumpFactoryAddress = "0x83b52fb6ED1C5Fb9b313cD5b1D4FDbd37766F7Ef";
+	const PumpFactoryAddress = "0x44E1c4fcDE5dD693AF979b1fa4E47Eb3E107aCdc";
 
 	const PumpFactoryFactory = await ethers.getContractFactory("PumpCloneFactory");
 	// const PumpFactory = await PumpFactoryFactory.deploy(routerAddress) as PumpCloneFactory;
